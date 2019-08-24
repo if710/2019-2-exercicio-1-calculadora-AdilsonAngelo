d684bc454736716a6038451746b5cd4eee8f7f99
Adilson Angelo <aasj2@cin.ufpe.br>
2019-08-24 03:01:59 -0300

    Tarefa 1 - calculadora

--- calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
@@ -2,14 +2,102 @@ package br.ufpe.cin.android.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    As funções onSaveInstanceState e onRestoreInstanceState
+//    são usadas abaixo para guardar e recuperar os valores de text_calc e text_info
+//    na mudança de configuração respectivamente
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("text_calc", text_calc.text.toString())
+        outState.putString("text_info", text_info.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        text_calc.setText(savedInstanceState?.getString("text_calc") ?: "")
+        text_info.text = (savedInstanceState?.getString("text_info") ?: "")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+
+//        Abaixo todos os botoes são configurados para executar o requisitado
+
+        btn_9.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_9.text.toString())
+        }
+        btn_8.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_8.text.toString())
+        }
+        btn_7.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_7.text.toString())
+        }
+        btn_6.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_6.text.toString())
+        }
+        btn_5.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_5.text.toString())
+        }
+        btn_4.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_4.text.toString())
+        }
+        btn_3.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_3.text.toString())
+        }
+        btn_2.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_2.text.toString())
+        }
+        btn_1.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_1.text.toString())
+        }
+        btn_0.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_0.text.toString())
+        }
+        btn_Add.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_Add.text.toString())
+        }
+        btn_Subtract.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_Subtract.text.toString())
+        }
+        btn_Multiply.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_Multiply.text.toString())
+        }
+        btn_Divide.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_Divide.text.toString())
+        }
+        btn_LParen.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_LParen.text.toString())
+        }
+        btn_RParen.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_RParen.text.toString())
+        }
+        btn_Dot.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_Dot.text.toString())
+        }
+        btn_Power.setOnClickListener{
+            text_calc.setText(text_calc.text.toString() + btn_Power.text.toString())
+        }
+        btn_Equal.setOnClickListener{
+//            tratemento de possíveis erros na validação da string de entrada da calculadora e colocando um toast para avisar usuario
+            try {
+                text_info.text = eval(text_calc.text.toString()).toString()
+            } catch (e: RuntimeException) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+        btn_Clear.setOnClickListener{
+            text_calc.setText("")
+            text_info.text = ""
+        }
+    }
 
     //Como usar a função:
     // eval("2+2") == 4.0
